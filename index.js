25e7859b196af1282233c6aa4d0d6bf85b16cfdd
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-23 00:44:03 +0300

    add static methods

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 class Formatter {
-  //add static methods here
+  // Capitalize
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+  //  Sanitize
+static sanitize(string) {
+  return string.replace(/[^a-zA-Z0-9-' ]/g, '');
+}
 }
